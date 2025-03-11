4cbf84c2d4e3958f9bab43644abf4481c11e28ee
minjoonbyun <bms0615@gmail.com>
2025-03-11 17:24:44 +0900

    Level3
    - Level3 completed
    - Kiosk Class created (start method)
    - Main class edit

--- KioskProject/src/com/kiosk/Kiosk.java
+++ KioskProject/src/com/kiosk/Kiosk.java
@@ -1,4 +1,36 @@
 package com.kiosk;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Kiosk {
+    private List<MenuItem> menuItems;
+
+    public Kiosk(List<MenuItem> menuItems) {
+        this.menuItems = menuItems;
+    }
+
+    public void start() {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.println("[ SHAKESHACK MENU ]");
+            for (int i =0; i<menuItems.size();i++) {
+                System.out.println((i+1)+". "+ menuItems.get(i).getName()+"| W"+ menuItems.get(i).getPrice() +"|"+ menuItems.get(i).getDescription());
+            }
+            System.out.println("0. 종료      | 종료");
+            System.out.println("메뉴를 선택하세요: ");
+            int choice = sc.nextInt();
+
+            if (choice == 0) {
+                System.out.println("프로그램을 종료합니다.");
+                break;
+            }
+            else if (choice >= 1 && choice <= menuItems.size()) {
+                System.out.println("선택한 메뉴:" +(choice)+". "+ menuItems.get(choice-1).getName()+"| W"+ menuItems.get(choice-1).getPrice() +"|"+ menuItems.get(choice-1).getDescription());
+            }
+            else {
+                System.out.println("유효하지 않은 입력입니다.");
+            }
+        }
+    }
 }
--- KioskProject/src/com/kiosk/Main.java
+++ KioskProject/src/com/kiosk/Main.java
@@ -13,27 +13,8 @@ public class Main {
         menuItems.add(new MenuItem("Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
         menuItems.add(new MenuItem("Hamburger",5.4,"비프패티를 기반으로 야채가 들어간 기본버거"));
 
-        Scanner sc = new Scanner(System.in);
-
-        while (true) {
-            System.out.println("[ SHAKESHACK MENU ]");
-            for (int i =0; i<menuItems.size();i++) {
-                System.out.println((i+1)+". "+ menuItems.get(i).getName()+"| W"+ menuItems.get(i).getPrice() +"|"+ menuItems.get(i).getDescription());
-            }
-            System.out.println("0. 종료      | 종료");
-            System.out.println("메뉴를 선택하세요: ");
-            int choice = sc.nextInt();
-
-            if (choice == 0) {
-                System.out.println("프로그램을 종료합니다.");
-                break;
-            }
-            else if (choice >= 1 && choice <= menuItems.size()) {
-                System.out.println("선택한 메뉴:" +(choice)+". "+ menuItems.get(choice-1).getName()+"| W"+ menuItems.get(choice-1).getPrice() +"|"+ menuItems.get(choice-1).getDescription());
-            }
-            else {
-                System.out.println("유효하지 않은 입력입니다.");
-            }
-        }
+        //Kiosk 객체를 생성
+        Kiosk kiosk = new Kiosk(menuItems);
+        kiosk.start();
     }
 }
