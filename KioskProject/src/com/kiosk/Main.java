775166daef4817d08341bc52abae5faa743910f2
minjoonbyun <bms0615@gmail.com>
2025-03-11 15:54:05 +0900

    Level2
    - level2 completed
    - Make List & Main class Edit

--- KioskProject/src/com/kiosk/Main.java
+++ KioskProject/src/com/kiosk/Main.java
@@ -1,25 +1,38 @@
 package com.kiosk;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        List<MenuItem> menuItems = new ArrayList<MenuItem>();
+
+        menuItems.add(new MenuItem("ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
+        menuItems.add(new MenuItem("SmokeShack", 8.9, "베이컨과 체리 페퍼가 들어간 스파이시 치즈버거"));
+        menuItems.add(new MenuItem("Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
+        menuItems.add(new MenuItem("Hamburger",5.4,"비프패티를 기반으로 야채가 들어간 기본버거"));
+
         Scanner sc = new Scanner(System.in);
 
         while (true) {
             System.out.println("[ SHAKESHACK MENU ]");
-            System.out.println("1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거");
-            System.out.println("2. SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
-            System.out.println("3. Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
-            System.out.println("4. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거");
+            for (int i =0; i<menuItems.size();i++) {
+                System.out.println((i+1)+". "+ menuItems.get(i).getName()+"| W"+ menuItems.get(i).getPrice() +"|"+ menuItems.get(i).getDescription());
+            }
             System.out.println("0. 종료      | 종료");
-
             System.out.println("메뉴를 선택하세요: ");
             int choice = sc.nextInt();
 
             if (choice == 0) {
                 System.out.println("프로그램을 종료합니다.");
                 break;
+            }
+            else if (choice >= 1 && choice <= menuItems.size()) {
+                System.out.println("선택한 메뉴:" +(choice)+". "+ menuItems.get(choice-1).getName()+"| W"+ menuItems.get(choice-1).getPrice() +"|"+ menuItems.get(choice-1).getDescription());
+            }
+            else {
+                System.out.println("유효하지 않은 입력입니다.");
             }
         }
     }
--- KioskProject/src/com/kiosk/MenuItem.java
+++ KioskProject/src/com/kiosk/MenuItem.java
@@ -4,4 +4,32 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class MenuItem {
+    private String name;
+    private double price;
+    private String description;
+
+    MenuItem(String name, double price, String description) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
